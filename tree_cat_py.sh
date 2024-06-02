e47016c41220dc0e189f373adf533c5c0cdedfd9
iagolirapassos <iagolirapassos@gmail.com>
2024-06-02 11:29:28 -0300

    README added

--- tree_cat_py.sh
+++ tree_cat_py.sh
@@ -5,16 +5,36 @@ base_dir=$(pwd)
 
 # Lista de arquivos .py para imprimir o nome e o conteúdo
 files=(
-    "brazbot/bot.py"
-    "brazbot/cache.py"
-    "brazbot/commands.py"
-    "brazbot/decorators.py"
-    "brazbot/events.py"
-    "brazbot/file.py"
-    "brazbot/responses.py"
-    "brazbot/utils.py"
-    "setup.py"
-    "simple_example_interactions.py"
+    "brazbot/argument_descriptions.py"
+│   "brazbot/attachments.py"
+│   "brazbot/autocomplete.py"
+│   "brazbot/bot_itimized.py"
+│   "brazbot/bot.py"
+│   "brazbot/buttons.py"
+│   "brazbot/cache.py"
+│   "brazbot/channels.py"
+│   "brazbot/cogs.py"
+│   "brazbot/commands.py"
+│   "brazbot/decorators.py"
+│   "brazbot/dropdowns.py"
+│   "brazbot/embed.py"
+│   "brazbot/events.py"
+│   "brazbot/file.py"
+│   "brazbot/forms.py"
+│   "brazbot/greedy_union.py"
+│   "brazbot/guilds.py"
+│   "brazbot/__init__.py"
+│   "brazbot/literal_arguments.py"
+│   "brazbot/member.py"
+│   "brazbot/message_handler.py"
+│   "brazbot/messages.py"
+│   "brazbot/optional_arguments.py"
+│   "brazbot/responses.py"
+│   "brazbot/roles.py"
+│   "brazbot/snowflake.py"
+│   "brazbot/subgroups.py"
+│   "brazbot/threads.py"
+│   "brazbot/utils.py"
 )
 
 # Itera sobre cada arquivo na lista
@@ -33,4 +53,4 @@ do
     else
         echo "Arquivo não encontrado: $file"
     fi
-done
+done > tree_cat_view.txt
